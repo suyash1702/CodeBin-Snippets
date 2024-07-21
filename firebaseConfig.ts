c94ff2f8ca6f80222f96f98c127e47d76c0d1a88
Suyash Mungase <ssmungase2580@gmail.com>
2024-07-21 07:58:11 +0530

    Add files via upload

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA14G6UMdcWuXbG1BVtoi7lP0sXE4ENZyA",
+  authDomain: "codebin-suyash1702.firebaseapp.com",
+  projectId: "codebin-suyash1702",
+  storageBucket: "codebin-suyash1702.appspot.com",
+  messagingSenderId: "975702710358",
+  appId: "1:975702710358:web:ac5ed505a8095f70d543f7",
+  measurementId: "G-FE7G20HY9N"
+};
